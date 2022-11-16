639909b5bbd6909d5f4b3dd2d82c27fcc70eed88
Saad Zubairi <saadhzubairi@outlook.com>
2022-11-16 10:31:34 +0500

    simple changes

---
+++ lib/login_signup_pages/loginScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("hey!"),
+      ),
+    );
+  }
+}
